3cc8857530bb6b40a109fff7cdbd58e5b7be31a8
Jess Bird <jessannbird@gmail.com>
2017-11-23 21:26:09 +0000

    updated readme

--- dist/scripts/controllers/homeController.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    function homeController(Room) {
-        // no functionality needed here yet
-    }
-    
-    angular.module('chateau')
-        .controller('homeController', ['Room', homeController]);
-})();
